1b2f3616fd21c9a4e00b632785be4f215eb9bf60
Al Siam <alsiam.dev@gmail.com>
2023-03-12 16:45:38 +0600

    SEO updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import Terminal from "@/components/Terminal";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,12 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>Al Siam - Terminal Based Developer Portfolio</title>
+        <meta name="description" content="Welcome to Al Siam's developer portfolio! Explore Al's skills, projects, and background through this terminal-based portfolio. Try typing in commands like 'help', 'projects', 'about', and 'skills' to learn more about Al and the work as a developer" />
+        <meta name="keywords" content="HTML, CSS, JavaScript, TypeScript, Saif Al Siam, Al Siam, alsiam, " />
+        <meta name="author" content="Al Siam" />
+      </Head>
       <Terminal />
     </>
   );
